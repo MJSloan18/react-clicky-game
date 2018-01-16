d4cf0655af9e8422a2b7295b184d379bd3707c24
Michael Sloan <m.j.sloan88@gmail.com>
2018-01-16 02:14:15 +0000

    Updated characters.json file

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -5,6 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+          <h5>Wins:</h5>
+          <h5>Losses:</h5>
         <header className="App-header">
           <img src="https://pbs.twimg.com/profile_images/913102538081148928/58i0mJwi_400x400.jpg" className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to The Clicky Game</h1>
