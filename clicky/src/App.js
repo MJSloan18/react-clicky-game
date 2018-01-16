d609e279c70b0996dd5ced5862bcedeb7ded2988
Michael Sloan <m.j.sloan88@gmail.com>
2018-01-16 01:45:41 +0000

    Updated App home page

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +6,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src="https://pbs.twimg.com/profile_images/913102538081148928/58i0mJwi_400x400.jpg" className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to The Clicky Game</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Select a Game Card below, without choosing the same one twice. Every correct guess will log a win, if you guess the same card more than once, a loss will register and the game restarts</h1>
+       
       </div>
     );
   }
