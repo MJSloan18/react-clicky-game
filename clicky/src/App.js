f07cda02a41e7758e006f7af9a5efd9e6fc9bc9a
Michael Sloan <m.j.sloan88@gmail.com>
2018-01-16 02:51:39 +0000

    Oakland members load to the page, not clickable, need to edit Oakland.css file

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
+import OaklandCard from "./components/OaklandCard/Oakland.js";
 import './App.css';
+import members from "./oakland.json";
 
 class App extends Component {
+
+  state = {
+    members
+  };
+
+  removeMember = id => {
+    const members = this.state.members.filter(members.id !== id);
+    this.setState({ members });
+  }
+
   render() {
     return (
       <div className="App">
-          <h5>Wins:</h5>
-          <h5>Losses:</h5>
         <header className="App-header">
           <img src="https://pbs.twimg.com/profile_images/913102538081148928/58i0mJwi_400x400.jpg" className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to The Clicky Game</h1>
         </header>
         <h1>Select a Game Card below, without choosing the same one twice. Every correct guess will log a win, if you guess the same card more than once, a loss will register and the game restarts</h1>
+        
+        <title>Oakland Raiders</title>
+        {this.state.members.map(members => (
+          <OaklandCard
+            removeMember={this.removeMember}
+            id={members.id}
+            key={members.id}
+            name={members.name}
+            image={members.image}
+            position={members.position}
+          />
+        
+        ))}
        
       </div>
     );
---
+++ clicky/src/components/OaklandCard/Oakland.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const OaklandCard = props => (
+
+    <div className="card">
+            <img alt={props.name} src={props.image} />
+        
+        <div className="content">
+            <ul>
+                <li>Name:{props.name}</li>
+                <li>Position:{props.position}</li>
+            </ul>
+        </div>
+        
+        <span onClick={() => props.removeMember(props.id)} className="remove">x</span>
+        
+    </div>
+
+
+);
+
+export default OaklandCard;
